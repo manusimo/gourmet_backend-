87a30b92421146950a454a744667a3d8454fe29f
manuel <onwaxcomm@gmail.com>
2024-11-26 15:28:31 -0300

    agregar

--- src/index.js
+++ src/index.js
@@ -33,5 +33,5 @@ app.use('/api', poolRoutes)
 
 const PORT = process.env.PORT; 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
