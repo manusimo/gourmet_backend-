b8885c0c94df06e21de8c4a160587eb96d528790
mvergarab <onwaxcomm@gmail.com>
2025-12-01 17:26:12 -0300

    Fix migration: remove Conversation changes and DROP INDEX line

--- prisma/migrations/20251021200445_add_deleted_at_to_restaurant/migration.sql
+++ prisma/migrations/20251021200445_add_deleted_at_to_restaurant/migration.sql
@@ -1,8 +1,6 @@
 -- AlterTable
 ALTER TABLE "Employee" ALTER COLUMN "birthDate" SET DEFAULT timestamp '1996-02-02 00:00:00';
 
--- AlterTable
-ALTER TABLE "Restaurant" ADD COLUMN     "deletedAt" TIMESTAMP(3);
 
 -- CreateTable
 CREATE TABLE "KnowledgeDocument" (
